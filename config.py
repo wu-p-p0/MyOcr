fc2d43674094155abac136b42147ae3b02266129
wuppo <1601962919@qq.com>
2025-05-10 13:25:38 +0800

    test

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # Basic config
 lang_list = ['ch_sim', 'en']
 use_GPU = True
-model_storage_directory = None  # if None: "~/.EasyOCR/."
+model_storage_directory = "./model"  # if None: "~/.EasyOCR/."
 
 # Image reader config
 detail = 0  # Set this to 0 for simple output
 paragraph = True
 rotation_info = None  # should be list
-batch_size = 1
+batch_size = 2
